c01fa5ac5a15017e24afc7063aa7e0314669db33
cmdr2 <secondary.cmdr2@gmail.com>
2025-09-02 14:52:44 +0530

    Center buttons

--- screenrecorder/ui_buttons.py
+++ screenrecorder/ui_buttons.py
@@ -23,6 +23,20 @@ class UIButtonPanel:
         self.select_btn.pack(side="left", padx=10)
 
     def show(self):
+        self.button_win.update_idletasks()
+        parent = self.button_win.master
+        sw = parent.winfo_screenwidth()
+        sh = parent.winfo_screenheight()
+        win_w = self.button_win.winfo_width()
+        win_h = self.button_win.winfo_height()
+        # If width/height are 1 (not yet rendered), estimate from buttons
+        if win_w <= 1:
+            win_w = self.record_btn.winfo_reqwidth() + self.select_btn.winfo_reqwidth() + 40
+        if win_h <= 1:
+            win_h = max(self.record_btn.winfo_reqheight(), self.select_btn.winfo_reqheight()) + 20
+        x = (sw - win_w) // 2
+        y = int(sh * 0.9 - win_h // 2)
+        self.button_win.geometry(f"{win_w}x{win_h}+{x}+{y}")
         self.button_win.deiconify()
         self.button_win.lift()
 
