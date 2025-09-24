b3bf8726f1ce8e53bc21f4626aa7f795f2dd4ef6
cmdr2 <secondary.cmdr2@gmail.com>
2025-09-24 14:55:06 +0530

    Homepage

--- screenrecorder/tray.py
+++ screenrecorder/tray.py
@@ -7,10 +7,19 @@ def create_tray_icon(overlay_app):
         icon.stop()
         overlay_app.root.after(0, overlay_app.root.quit)
 
+    def open_project_homepage(icon, item):
+        import webbrowser
+
+        webbrowser.open("https://github.com/cmdr2/screenrecorder/")
+
     image = Image.new("RGB", (64, 64), (0, 0, 0))
     d = ImageDraw.Draw(image)
     d.rectangle([16, 16, 48, 48], fill=(255, 0, 0))
 
-    menu = pystray.Menu(pystray.MenuItem("Quit", quit_app))
+    menu = pystray.Menu(
+        pystray.MenuItem("Project Homepage", open_project_homepage),
+        pystray.Menu.SEPARATOR,
+        pystray.MenuItem("Quit", quit_app),
+    )
 
     return pystray.Icon("recorder", image, "Recorder", menu)
